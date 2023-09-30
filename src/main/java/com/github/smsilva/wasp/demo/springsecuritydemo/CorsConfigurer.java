471b2254390d2d378cd1e92d29dea80cdc334dea
Silvio Silva <smsilva@gmail.com>
2023-09-30 20:02:48 -0300

    feat: CORS Restricted using Bean

---
+++ src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/CorsConfigurer.java
@@ -0,0 +1,25 @@
+package com.github.smsilva.wasp.demo.springsecuritydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer {
+
+    @Bean
+    WebMvcConfigurer cors() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+
+        };
+    }
+
+}
--- src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/UserController.java
+++ src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/UserController.java
@@ -1,10 +1,8 @@
 package com.github.smsilva.wasp.demo.springsecuritydemo;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 class UserController {
 
