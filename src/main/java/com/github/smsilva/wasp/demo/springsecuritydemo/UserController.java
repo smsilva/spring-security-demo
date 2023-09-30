3b678142512e2262abbeae673ff87acbd2f51bde
Silvio Silva <smsilva@gmail.com>
2023-09-30 19:51:10 -0300

    feat: CORS Restricted

--- src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/UserController.java
+++ src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/UserController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 class UserController {
 
-    @CrossOrigin
     @GetMapping("/me")
     String me() {
         return "John Doe";
     }
-    
+
 }
