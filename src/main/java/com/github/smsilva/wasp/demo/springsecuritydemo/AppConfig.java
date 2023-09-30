5ec65f8a1b14cf0bc510e9354502695cc980e404
Silvio Silva <smsilva@gmail.com>
2023-09-30 20:36:00 -0300

    feat: CORS Restricted using Bean

--- src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/AppConfig.java
+++ src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfigurer {
+public class AppConfig {
 
     @Bean
     WebMvcConfigurer cors() {
@@ -21,5 +21,6 @@ public class CorsConfigurer {
 
         };
     }
+
 
 }
