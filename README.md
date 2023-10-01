14234c34c1ccb3f0578ce0b0bcda9ced32393f30
Silvio Silva <smsilva@gmail.com>
2023-10-01 10:46:40 -0300

    feat: Spring Security Enabled

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ```bash
 curl -i http://localhost:8080/greetings/hi
 
-export PASSWORD=8c609a91-f9ee-4458-86c1-7df549935b2a
+export PASSWORD=596ae797-54af-4839-9a25-e746ce9164c1
 curl \
   -u user:${PASSWORD?} \
   -i http://localhost:8080/greetings/hi
--- src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/AppConfig.java
+++ src/main/java/com/github/smsilva/wasp/demo/springsecuritydemo/AppConfig.java
@@ -1,11 +1,18 @@
 package com.github.smsilva.wasp.demo.springsecuritydemo;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
 
     @Bean
@@ -15,12 +22,28 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*");
+                        .allowedHeaders("Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
             }
 
         };
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(cors -> cors
+                        .disable())
+                .csrf(csrf -> csrf
+                        .disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/me").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .build();
+    }
 
 }
